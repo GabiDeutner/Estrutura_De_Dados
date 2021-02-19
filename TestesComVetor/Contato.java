93e9298324340acfd218e97ab03e88dea98f6df3
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-19 20:03:59 -0300

    Update Contato.java

---
+++ TestesComVetor/Contato.java
@@ -0,0 +1,72 @@
+package com.gabi.vetor.teste;
+
+import java.util.Objects;
+
+public class Contato {
+
+    
+    private String nome;
+    private String telefone;
+    private String email;
+//operador diamond e <T> type define o tipo de vetor
+    Contato(String contato_1, String string, String contato1emailcom) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString() {
+        return "Contato{" + "nome=" + nome + ", telefone=" + telefone + ", email=" + email + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.nome);
+        hash = 79 * hash + Objects.hashCode(this.telefone);
+        hash = 79 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contato other = (Contato) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.telefone, other.telefone)) {
+            return false;
+        }
+        return Objects.equals(this.email, other.email);
+    }
+       
+}
