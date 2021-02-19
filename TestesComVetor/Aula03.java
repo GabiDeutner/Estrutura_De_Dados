e27eeea09caf09ce745bce38f790fd8a02114254
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-19 20:02:45 -0300

    Update Aula03.java

---
+++ TestesComVetor/Aula03.java
@@ -0,0 +1,16 @@
+
+package com.gabi.vetor.teste;
+
+import com.gabi.vetor.Vetor; // importei a classe vetor
+
+public class Aula03 {
+    public static void main(String[] args)throws Exception{
+        
+        Vetor vetor = new Vetor(5);
+        
+                vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+    }
+  
+}
