1cd592a53e95ee580734f0ff17c34e891fa2dc9d
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-19 20:02:35 -0300

    Update Aula02.java

---
+++ TestesComVetor/Aula02.java
@@ -0,0 +1,15 @@
+
+package com.gabi.vetor.teste;
+
+import com.gabi.vetor.Vetor;
+
+public class Aula02 {
+    
+    public static void main (String[] args){
+        Vetor vetor = new Vetor (5);
+        
+        int i=0; 
+        i++;
+    }
+    
+}
